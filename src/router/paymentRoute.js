269cd1a2ab5770839a30bae46b522b665ed2eb5c
NguyênNguyên <billbatri085@gmail.com>
2024-11-23 10:59:36 +0700

    RM-20 | Add Payment

--- src/router/paymentRoute.js
+++ src/router/paymentRoute.js
@@ -9,6 +9,7 @@ const initPaymentRoute = (app) => {
     // Định nghĩa các route thanh toán
     router.post('', paymentController.createPayment); // Tạo request thanh toán
     router.post('/callback', paymentController.callbackPayment); // Xử lý callback từ MoMo
+
     return app.use('/api/payment', router);
 }
 
